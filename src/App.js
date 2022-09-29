f641889630f12203fa73fd81816e476de586fbf6
Phil Metscher <philmetschermailt@gmx.de>
2022-09-29 16:04:57 +0200

    add simple markup, removed logo import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,43 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header>
+        <h1>React and Morty</h1>
+      </header>
+      <main>
+        <section>
+          <article>
+            <img
+              src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+              alt="Morty Smith"
+            />
+            <p>Morty Smith</p>
+            <button>Show more</button>
+          </article>
+        </section>
+      </main>
+      <footer>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Link1</a>
+            </li>
+            <li>
+              <a href="#">Link2</a>
+            </li>
+            <li>
+              <a href="#">Link3</a>
+            </li>
+            <li>
+              <a href="#">Link4</a>
+            </li>
+          </ul>
+        </nav>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
